2ef7a9a6ed640eb04178455b45b9b7542545d9ac
olivierdelacherie <olivier28111999@hotmail.fr>
2022-06-16 10:42:17 +0200

    ajout variable couleur + font

--- wp-content/themes/moonwine/functions.php
+++ wp-content/themes/moonwine/functions.php
@@ -136,3 +136,5 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
     $fragments['a.cart-customlocation'] = ob_get_clean();
     return $fragments;
 }
+
+add_theme_support( 'custom-logo' );
--- wp-content/themes/moonwine/header.php
+++ wp-content/themes/moonwine/header.php
@@ -8,13 +8,25 @@
 
 </head>
 <body <?= body_class() ?>>
+
 <header class="header" role="banner">
+    <div class="prenav">
+        <div class="container">
+            <div class="prenav-header">
+                <a class="custom-logo" href="<?php echo get_site_url(); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" alt="Logo">
+                </a>
+                <nav id="prenavbar" class="navbar" role="navigation">
+
+                    <a href="/mon-compte/edit-account/" class="luca-a-class">Détails du compte</a>
+                    <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
+                </nav>
+            </div>
+        </div>
+    </div>
     <nav id="navbar" class="navbar" role="navigation">
         <div class="container">
             <div id="navbar-header" class="navbar-header" data-open="false">
-                <a class="custom-logo" href="<?php echo get_site_url(); ?>">
-                    logo
-                </a>
                 <ul class="nav navbar-nav">
                     <?php
                     if ( has_nav_menu( 'header' ) ) {
@@ -29,7 +41,6 @@
 
                     }
                     ?>
-                    <a class="cart-customlocation" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><?php echo sprintf ( _n( '%d item', '%d items', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?> – <?php echo WC()->cart->get_cart_total(); ?></a>
                 </ul>
 
                 <button type="button" id="megamenu-mobile" class="navbar-toggle collapsed" data-toggle="collapse" aria-expanded="false"  aria-label="Navigation mobile">
